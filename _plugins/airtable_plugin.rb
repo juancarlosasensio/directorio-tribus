09a19c16e301a7e568608248d2fb654d1e19ba89
juancarlosasensio <juancarlos@page-bird.com>
2020-09-22 17:05:33 -0400

    WIP--find build bug

--- _plugins/airtable_plugin.rb
+++ _plugins/airtable_plugin.rb
@@ -18,7 +18,9 @@ puts "#{@records.count} records pulled from Airtable."
 
 File.open("_data/airtable.yml", "w") do |f|
   puts "Sanitizing records..."
-  data = @records.map { |record| record.fields }
+  data = @records.map { |record| record.fields }.each do |record|
+    record.keys.each { |k| record[ @column_name_mappings[k] ] = record.delete(k) if @column_name_mappings[k] || @column_name_mappings[k] }
+  end
   puts "All records have been sanitized."
   f.write(data.to_yaml)
   puts "YAML database successfully created!"
