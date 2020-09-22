9710e6dc08cc128d3b7bd88644489903336a88f8
juancarlosasensio <juancarlos@page-bird.com>
2020-09-22 12:55:26 -0400

    Revert "Add strings instead of ENV variables"
    This reverts commit 4d29ed38730e28093593ae09d2a6fc4d783d9ece.

--- _plugins/airtable_plugin.rb
+++ _plugins/airtable_plugin.rb
@@ -3,21 +3,21 @@ require 'airtable'
 require 'active_support/all'
 require 'colorize'
 
-@client = Airtable::Client.new('keyUQtss54qPKS32W')
-@table = @client.table('appzux6XBDCjO151J', 'DirectorioTribus'])
+@client = Airtable::Client.new(ENV['AIR_API_KEY'])
+@table = @client.table(ENV['AIR_BASE_ID'], ENV['AIR_BASE_NAME'])
 @records = @table.all
 @column_name_mappings = { "Nombre" => "name", "Categoría" => "category", "Descripción" => "description",
                          "Número de teléfono" => "phone", "Link de contacto directo (red social)" => "social_media", 
                          "Logo de tu negocio en formato .PNG o .JPG" => "logo_link" }   
 
-puts "#{@records.count} records pulled from Airtable.".cyan
+puts "#{@records.count} records pulled from Airtable."
 
 File.open("_data/airtable.yml", "w") do |f|
   puts "Sanitizing records..."
   data = @records.map { |record| record.fields }.each do |record|
     record.keys.each { |k| record[ @column_name_mappings[k] ] = record.delete(k) if @column_name_mappings[k] || @column_name_mappings[k] }
   end
-  puts "All records have been sanitized.".cyan
+  puts "All records have been sanitized."
   f.write(data.to_yaml)
-  puts "YAML database successfully created!".green
+  puts "YAML database successfully created!"
 end
