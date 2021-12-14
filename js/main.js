cae056176ac5ea218798e5718d0ff6f09f183505
Juan Carlos Asensio <jasensio@ufm.edu>
2021-12-14 13:09:59 -0500

    Refactor UI with web components

--- js/main.js
+++ js/main.js
@@ -29,3 +29,78 @@ $(document).on("click", "[data-filter-category]", function () {
     $(`[data-category='${category}']`).removeClass("hidden");
   }
 })
+
+/**
+ * module cluster-l
+ * description
+ * A custom element for grouping items, with control over the margin between them
+ * property {string} justify=flex-start A CSS `justify-content` value
+  display: block;
+  box-sizing: content-box;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: var(--measure);
+}
+*/
+
+class Cluster extends HTMLElement {
+  constructor() {
+    super();
+    this.render = () => {
+      this.i = `Cluster-${[this.justify, this.align, this.space].join('')}`;
+      this.dataset.i = this.i;
+      if (!document.getElementById(this.i)) {
+        let styleEl = document.createElement('style');
+        styleEl.id = this.i;
+        styleEl.innerHTML = `
+          [data-i="${this.i}"] {
+            justify-content: ${this.justify};
+            align-items: ${this.align};
+            gap: ${this.space};
+          }
+        `.replace(/\s\s+/g, ' ').trim();
+        document.head.appendChild(styleEl);
+      }
+    }
+  }
+
+  get justify() {
+    return this.getAttribute('justify') || 'flex-start';
+  }
+
+  set justify(val) {
+    return this.setAttribute('justify', val);
+  }
+
+  get align() {
+    return this.getAttribute('align') || 'flex-start';
+  }
+
+  set align(val) {
+    return this.setAttribute('align', val);
+  }
+
+  get space() {
+    return this.getAttribute('space') || 'var(--s1)';
+  }
+
+  set space(val) {
+    return this.setAttribute('space', val);
+  }
+
+  static get observedAttributes() {
+    return ['justify', 'align', 'space'];
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  attributeChangedCallback() {
+    this.render();
+  }
+}
+
+if ('customElements' in window) {
+  customElements.define('cluster-l', Cluster);
+}
